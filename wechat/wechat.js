3f2768ff0d555f732fb7822efcba0a2b5c0034ab
zangtengfei <81709456@qq.com>
2019-03-22 15:26:04 +0800

    bugfix

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -1,28 +1,21 @@
 'use strict' //设置为严格模式
 
-const crypto = require('crypto'), //引入加密模块
-       https = require('https'), //引入 htts 模块
-        util = require('util'), //引入 util 工具包
-accessTokenJson = require('./accessToken'); //引入本地存储的 access_token
-
-
 //构建 WeChat 对象 即 js中 函数就是对象
 var WeChat = function(config){
-  //设置 WeChat 对象属性 config
-  this.config = config;
-  //设置 WeChat 对象属性 token
-  this.token = config.token;
-  //设置 WeChat 对象属性 appID
-  this.appID = config.appID;
-  //设置 WeChat 对象属性 appScrect
-  this.appScrect = config.appScrect;
-  //设置 WeChat 对象属性 apiDomain
-  this.apiDomain = config.apiDomain;
-  //设置 WeChat 对象属性 apiURL
-  this.apiDomain = config.apiURL;
+    //设置 WeChat 对象属性 config
+    this.config = config;
+    //设置 WeChat 对象属性 token
+    this.token = config.token;
+    //设置 WeChat 对象属性 appID
+    this.appID = config.appID;
+    //设置 WeChat 对象属性 appScrect
+    this.appScrect = config.appScrect;
+    //设置 WeChat 对象属性 apiDomain
+    this.apiDomain = config.apiDomain;
+    //设置 WeChat 对象属性 apiURL
+    this.apiDomain = config.apiURL;
 
-  //用于处理 https Get请求方法
-  this.requestGet = function(url){
+    this.requestGet = function(url){
       return new Promise(function(resolve,reject){
           https.get(url,function(res){
               var buffer = [],result = "";
@@ -42,7 +35,32 @@ var WeChat = function(config){
       });
   }
 }
+/**
+ * 微信接入验证
+ */
+WeChat.prototype.auth = function(req,res){
+  //1.获取微信服务器Get请求的参数 signature、timestamp、nonce、echostr
+     var signature = req.query.signature,//微信加密签名
+         timestamp = req.query.timestamp,//时间戳
+             nonce = req.query.nonce,//随机数
+         echostr = req.query.echostr;//随机字符串
 
+     //2.将token、timestamp、nonce三个参数进行字典序排序
+     var array = [this.token,timestamp,nonce];
+     array.sort();
+
+     //3.将三个参数字符串拼接成一个字符串进行sha1加密
+     var tempStr = array.join('');
+     const hashCode = crypto.createHash('sha1'); //创建加密类型 
+     var resultCode = hashCode.update(tempStr,'utf8').digest('hex'); //对传入的字符串进行加密
+
+     //4.开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+     if(resultCode === signature){
+         res.send(echostr);
+     }else{
+         res.send('mismatch');
+     }
+}
 /**
  * 获取微信 access_token
  */
@@ -75,6 +93,5 @@ WeChat.prototype.getAccessToken = function(){
       }
   });
 }
-
 //暴露可供外部访问的接口
 module.exports = WeChat;
