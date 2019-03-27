92312f6d30308e7ed7770e409142218e8c748e7d
zangtengfei <81709456@qq.com>
2019-03-27 17:55:04 +0800

    增加user.html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express'), //express 框架 
-      wechat  = require('./wechat/wechat'), 
-       config = require('./config');//引入配置文件
-       
+  wechat = require('./wechat/wechat'),
+  config = require('./config');//引入配置文件
+
 var app = express();//实例express框架
 
 var wechatApp = new wechat(config); //实例wechat 模块
@@ -10,30 +10,30 @@ var wechatApp = new wechat(config); //实例wechat 模块
 app.use(express.static('public'));
 
 //用于处理所有进入 3000 端口 get 的连接请求
-app.get('/',function(req,res){
-    wechatApp.auth(req,res);
+app.get('/', function (req, res) {
+  wechatApp.auth(req, res);
 });
 
 //用于处理所有进入 3000 端口 post 的连接请求
-app.post('/',function(req,res){
-    wechatApp.handleMsg(req,res);
+app.post('/', function (req, res) {
+  wechatApp.handleMsg(req, res);
 });
 
 //用于请求获取 access_token
-app.get('/getAccessToken',function(req,res){
-    wechatApp.getAccessToken().then(function(data){
-        res.send(data);
-    });
+app.get('/getAccessToken', function (req, res) {
+  wechatApp.getAccessToken().then(function (data) {
+    res.send(data);
+  });
 });
 
 // 用于请求获取userinfo
-app.get('/wx_login',function(req,res){
-    wechatApp.wxLogin(req, res);
+app.get('/wx_login', function (req, res) {
+  wechatApp.wxLogin(req, res);
 });
 
 
-app.get('/get_wx_access_token', function(req,res, next){
-    wechatApp.getWxAccessToken(req, res);
+app.get('/get_wx_access_token', function (req, res, next) {
+  wechatApp.getWxAccessToken(req, res);
 });
 
 
