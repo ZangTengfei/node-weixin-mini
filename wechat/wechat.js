3a5c586835a5d8ef20564c358eb3408513be60c9
zangtengfei <81709456@qq.com>
2019-03-28 17:21:13 +0800

    bugfix

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -375,7 +375,8 @@ WeChat.prototype.getWxJssdkConfig = function (req, res) {
     },
     function (error, response, body) {
       if(response) {
-        jsapi_ticket = response.ticket;
+        console.log(response.body);
+        jsapi_ticket = response.body.ticket;
         var noncestr = Math.random().toString(36).substr(2);
         var timestamp = parseInt((new Date()).valueOf()/1000);
         var str = 'jsapi_ticket='+jsapi_ticket+'&noncestr='+noncestr+'&timestamp='+timestamp+'&url=http://wxapi.zangtengfei.com/user.html';
