e8a05d8b41f73017563b756452965da124c9558a
zangtengfei <81709456@qq.com>
2019-03-28 17:42:33 +0800

    bugfix

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -363,34 +363,35 @@ WeChat.prototype.getWxAccessToken = function (req, res) {
 
 WeChat.prototype.getWxJssdkConfig = function (req, res) {
   var that = this;
-
-  var access_token = accessTokenJson.access_token;
-  var jsapi_ticket = '';
-  var url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+access_token+'&type=jsapi';
-  that.requestGet(url).then(
-    function (response) {
-      if(response) {
-        console.log(response);
-        // res.send(response);
-        jsapi_ticket = response.ticket;
-        var noncestr = Math.random().toString(36).substr(2);
-        var timestamp = parseInt((new Date()).valueOf()/1000);
-        var str = 'jsapi_ticket='+jsapi_ticket+'&noncestr='+noncestr+'&timestamp='+timestamp+'&url=http://wxapi.zangtengfei.com/user.html';
-        const hashCode = crypto.createHash('sha1'); //创建加密类型 
-        var signature = hashCode.update(str, 'utf8').digest('hex'); //对传入的字符串进行加密
-        res.send({
-          response: response,
-          ticket: jsapi_ticket||'no_ticket',
-          appId: that.appID,
-          timestamp: timestamp,
-          nonceStr: noncestr,
-          signature: signature,
-          jsApiList: ['onMenuShareTimeline'],
-        });
-      }else {
-        console.log('获取jsapi_ticket失败');
-      }
-    });
+  this.getAccessToken().then(function (data) {
+    var access_token = data;
+    var jsapi_ticket = '';
+    var url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+access_token+'&type=jsapi';
+    that.requestGet(url).then(
+      function (response) {
+        if(response) {
+          console.log(response);
+          // res.send(response);
+          jsapi_ticket = response.ticket;
+          var noncestr = Math.random().toString(36).substr(2);
+          var timestamp = parseInt((new Date()).valueOf()/1000);
+          var str = 'jsapi_ticket='+jsapi_ticket+'&noncestr='+noncestr+'&timestamp='+timestamp+'&url=http://wxapi.zangtengfei.com/user.html';
+          const hashCode = crypto.createHash('sha1'); //创建加密类型 
+          var signature = hashCode.update(str, 'utf8').digest('hex'); //对传入的字符串进行加密
+          res.send({
+            response: response,
+            ticket: jsapi_ticket||'no_ticket',
+            appId: that.appID,
+            timestamp: timestamp,
+            nonceStr: noncestr,
+            signature: signature,
+            jsApiList: ['onMenuShareTimeline'],
+          });
+        }else {
+          console.log('获取jsapi_ticket失败');
+        }
+      });
+  });
 }
 
 //暴露可供外部访问的接口
