05b7c7c361ceb518af81546bc32cc2912cc467ab
zangtengfei <81709456@qq.com>
2019-03-28 17:08:52 +0800

    bugfix

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -383,6 +383,7 @@ WeChat.prototype.getWxJssdkConfig = function (req, res) {
         const hashCode = crypto.createHash('sha1'); //创建加密类型 
         var signature = hashCode.update(str, 'utf8').digest('hex'); //对传入的字符串进行加密
         res.send({
+          ticket: jsapi_ticket,
           appId: that.appID,
           timestamp: timestamp,
           nonceStr: noncestr,
